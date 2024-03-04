7ef6452b08c0a41aa48d9dc8fe1041648a82ef73
mdJasim2023 <jasim127md@gmail.com>
2024-03-04 17:38:34 +0530

    dg

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 import ShopppingProducts from './ShopppingProducts';
 import store from './store/store';
 import { Provider } from 'react-redux';
-import AddCarts from './AddCarts';
 import { Outlet } from 'react-router';
-import { Link } from 'react-router-dom';
 
 function App() {
   return (
